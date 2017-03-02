8e936e1f266b2e3440ccd9d6fd8d865b1174cfee
Your Name <andrius.kulbis@ktu.lt>
2017-03-02 09:39:54 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import Button from 'react-bootstrap/lib/Button';
 
-class App extends React.Component {
-	render() {
-		return (
-			<div>Hello React!</div>
-		)
-	}
+class App extends Component {
+    render() {
+        return (<div className="app">
+            <h1>Hello worldd</h1>
+            <Button bsStyle="danger">Test</Button>
+        </div>);
+    }
 }
 
-render(<App/>, document.getElementById('app'));
+render((<App/>), document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
 var webpack = require('webpack'),
-    path = require('path');
+    path    = require('path');
 
-var APP_DIR = path.resolve(__dirname, 'src');
+var NODE_MODULES = path.resolve(__dirname, 'node_modules');
+var BUILD_PATH   = path.resolve(__dirname, 'public', 'build');
+var INDEX_PATH   = path.resolve(__dirname, 'src', 'index.js');
 
 module.exports = {
+    devtool: 'eval',
     entry: [
-        'webpack-hot-middleware/client',
-        APP_DIR + '/index'
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080/',
+        INDEX_PATH
     ],
     output: {
-        path: path.resolve(__dirname, '/dist'),
+        path: BUILD_PATH,
         filename: 'bundle.js',
-        publicPath: '/static/'
+        publicPath: '/build/'
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
     module: {
         loaders: [{
             test: /\.js?/,
-            exclude: /node_modules/,
-            loaders: ['react-hot-loader', 'babel-loader']
+            loaders: ['react-hot-loader', 'babel-loader'],
+            exclude: [NODE_MODULES],
         }]
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js']
     },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
     externals: {
         'cheerio': 'window',
         'react/addons': 'react',
@@ -33,3 +35,4 @@ module.exports = {
         'react/lib/ReactContext': 'react',
     }
 };
+
